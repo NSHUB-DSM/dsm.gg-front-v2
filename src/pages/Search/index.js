9902e40e71f3a7caaf445eb2a77347725d15001d
woosang <woosang3078@hanmail.net>
2023-01-07 22:14:23 +0900

    ⚡ :: 메인 로고 클릭 시 홈 화면으로 이동

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 import Header from '../../components/Header/header';
 import logo from '../../assets/imgs/searchGameLogo.png';
 import Search from '../../components/Search';
+import useTitle from '../../components/Title/hook';
 
 export default function SearchPage() {
+    const titleUpdater = useTitle("불러오는 중...");
+    setTimeout(() => titleUpdater("소환사 전적 검색 - DSM.GG"))
 
     return (
         <Background>
