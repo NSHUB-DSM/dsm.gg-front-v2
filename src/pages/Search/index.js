55e7048bebeaf24f55a6fd98d2fed9aedfcc775d
woosang <woosang3078@hanmail.net>
2023-01-07 20:24:23 +0900

    🎓 :: 로고 스타일 변경

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/Header/header';
+import logo from '../../assets/imgs/searchGameLogo.png';
+import Search from '../../components/Search';
 
 export default function SearchPage() {
 
     return (
         <Background>
             <Header />
+            <LogoStyle>
+                <img src={logo} alt="로고 이미지" />
+            </LogoStyle>
+            <Search />
         </Background>
     )
 }
+
+const LogoStyle = styled.div`
+  margin-left: 607px;
+  margin-top: 80px;
+`
 
 const Background = styled.div`
   background-color: white;
