df4e70bfe12f7bf653ecb36b85e4b1a96fbf4f5c
woosang <woosang3078@hanmail.net>
2023-01-09 12:23:47 +0900

    ♻️ :: onclick 속성 리팩토링

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,41 +1,38 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
-import { ReactComponent as MainLogo } from '../../assets/svgs/mainLogo.svg';
-import useScrollFadeIn from './hooks';
-import { useNavigate } from 'react-router-dom';
-import useTitle from '../../components/Title/hook';
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as MainLogo } from "../../assets/svgs/mainLogo.svg";
+import useScrollFadeIn from "./hooks";
+import { useNavigate } from "react-router-dom";
+import useTitle from "../../components/Title/hook";
 
 export default function MainPage() {
-    const animationItem = useScrollFadeIn();
-    const navigate = useNavigate();
-    const onClick = () => {
-        navigate('/search');
-    }
+  const animationItem = useScrollFadeIn();
+  const navigate = useNavigate();
 
-    const titleUpdater = useTitle("불러오는 중...");
-    setTimeout(() => titleUpdater("DSM.GG"));
+  const titleUpdater = useTitle("불러오는 중...");
+  setTimeout(() => titleUpdater("DSM.GG"));
 
-    return (
-        <Background>
-            <MainLogoStyle />
-            <TextStyle>
-                <p>게임</p>
-                <p className="purple">전적 검색</p>
-                <p className="purple">실시간</p>
-                <p>매칭 서비스</p>
-            </TextStyle>
-            <MoveBtn onClick={onClick} {...animationItem}>
-                둘러보기
-            </MoveBtn>
-            <Space />
-        </Background>
-    )
+  return (
+    <Background>
+      <MainLogoStyle />
+      <TextStyle>
+        <p>게임</p>
+        <p className="purple">전적 검색</p>
+        <p className="purple">실시간</p>
+        <p>매칭 서비스</p>
+      </TextStyle>
+      <MoveBtn onClick={() => navigate("/search")} {...animationItem}>
+        둘러보기
+      </MoveBtn>
+      <Space />
+    </Background>
+  );
 }
 
 const Space = styled.div`
   min-width: 500px;
   min-height: 100px;
-`
+`;
 
 const MoveBtn = styled.button`
   width: 192px;
@@ -48,7 +45,7 @@ const MoveBtn = styled.button`
   margin-left: 742px;
   margin-top: 150px;
   cursor: pointer;
-`
+`;
 
 const easeIn = keyframes`
   0% {
@@ -59,7 +56,7 @@ const easeIn = keyframes`
   100% {
     opacity: 1;
   }
-`
+`;
 
 const TextStyle = styled.div`
   width: 916px;
@@ -69,24 +66,24 @@ const TextStyle = styled.div`
     font-weight: 600;
     margin: 0px;
     :nth-child(4) {
-        padding-left: 206px;
-        margin-top: -83px;
+      padding-left: 206px;
+      margin-top: -83px;
     }
   }
   .purple {
-    color: #7B5EB4;
+    color: #7b5eb4;
   }
   margin-left: 200px;
   margin-top: 314px;
   animation: ${easeIn} 1s ease-in;
-`
+`;
 
 const MainLogoStyle = styled(MainLogo)`
   margin-top: 44px;
   margin-left: 40px;
-`
+`;
 
 const Background = styled.div`
   width: 100vw;
   height: 100vh;
-`
+`;
