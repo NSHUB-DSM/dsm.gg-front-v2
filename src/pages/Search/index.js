a8416df300ca1261ef016f6c296f2594a9f38979
woosang <woosang3078@hanmail.net>
2023-01-09 12:23:57 +0900

    ♻️ :: 코드 리팩토링

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
-import Header from '../../components/Header/header';
-import logo from '../../assets/imgs/searchGameLogo.png';
-import Search from '../../components/Search';
-import useTitle from '../../components/Title/hook';
+import React from "react";
+import styled from "styled-components";
+import Header from "../../components/Header/header";
+import logo from "../../assets/imgs/searchGameLogo.png";
+import Search from "../../components/Search";
+import useTitle from "../../components/Title/hook";
 
 export default function SearchPage() {
-    const titleUpdater = useTitle("불러오는 중...");
-    setTimeout(() => titleUpdater("소환사 전적 검색 - DSM.GG"))
+  const titleUpdater = useTitle("불러오는 중...");
+  setTimeout(() => titleUpdater("소환사 전적 검색 - DSM.GG"));
 
-    return (
-        <>
-            <Header />
-            <LogoStyle>
-                <img src={logo} alt="로고 이미지" />
-            </LogoStyle>
-            <Search />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <LogoStyle>
+        <img src={logo} alt="로고 이미지" />
+      </LogoStyle>
+      <Search />
+    </>
+  );
 }
 
 const LogoStyle = styled.div`
   margin-left: 607px;
   margin-top: 80px;
-`
+`;
