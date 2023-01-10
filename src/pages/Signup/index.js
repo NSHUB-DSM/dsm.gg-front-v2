0a3acff6f92dc6b5da9741f046a63b4f535e0c31
woosang <woosang3078@hanmail.net>
2023-01-10 10:18:56 +0900

    ⚡ :: 회원가입 페이지 추가

---
+++ src/pages/Signup/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/Header/header';
+
+export default function SignupPage() {
+    return (
+        <>
+          <Header />
+        </>
+    )
+}
