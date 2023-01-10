0d7c23ef339ac6f3c2715138aae13c31780869a6
woosang <woosang3078@hanmail.net>
2023-01-10 17:19:51 +0900

    ⚡ :: 회원가입 input 컴포넌트 추가

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/Header/header';
+import SignupInput from '../../components/Signup';
 
 export default function SignupPage() {
     return (
         <>
           <Header />
+          <Title>회원가입</Title>
+          <Space />
+          <SignupInput />
         </>
     )
 }
+
+const Space = styled.div`
+  height: 70px;
+`
+
+const Title = styled.p`
+ font-size: 40px;
+ font-weight: 600;
+ text-align: center;
+ margin-top: 20px;
+`
