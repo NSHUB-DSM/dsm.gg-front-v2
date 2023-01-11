eb43cb0d6b512a00abb6cb6ef66f2ead5463f660
woosang <woosang3078@hanmail.net>
2023-01-11 10:31:51 +0900

    ⚡ :: 페이지 제목 할당

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/Header/header';
 import SignupInput from '../../components/Signup';
+import useTitle from '../../components/Title/hook';
 
 export default function SignupPage() {
+    const titleUpdater = useTitle("불러오는 중...");
+    setTimeout(() => titleUpdater("회원가입 - DSM.GG"));
+
     return (
         <>
           <Header />
