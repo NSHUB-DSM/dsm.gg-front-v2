c371f97990ed3cdc0d46387b27f68c8d1c63c8e9
조우상 <woosang3078@hanmail.net>
2023-01-16 15:57:03 +0900

    ⚡ :: 만들기 버튼 스크롤 애니메이션 속성 추가

--- src/pages/CreateMatch/index.js
+++ src/pages/CreateMatch/index.js
@@ -5,21 +5,58 @@ import useScrollFadeIn from "../Main/hooks";
 import useTitle from "../../components/Title/hook";
 
 export default function CreateMatch() {
+  const animationItem = useScrollFadeIn();
   const titleUpdater = useTitle("불러오는 중...")
   setTimeout(() => titleUpdater("매칭 생성 - DSM.GG"))
 
   return (
     <>
       <Header />
-      <Space />
+      <TopSpace />
       <div>
         <Title>매칭을 만들어 소통해보세요!</Title>
       </div>
+      <BottomSpace />
+      <BtnContainer {...animationItem}>
+        <CreateBtn>만들기</CreateBtn>
+      </BtnContainer>
     </>
   );
 }
 
-const Space = styled.div`
+const BtnContainer = styled.div`
+  width: 160px;
+  height: 60px;
+  margin: 0 auto;
+`
+
+const CreateBtn = styled.button`
+  width: 160px;
+  height: 60px;
+  background-color: black;
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 25px;
+  margin-top: -100px;
+`
+
+const BottomSpace = styled.div`
+  height: 500px;
+`
+
+const easeIn = keyframes`
+  0% {
+    opacity: 0;
+    transform: translate3d(20px);
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+const TopSpace = styled.div`
   height: 250px;
 `
 
@@ -27,5 +64,7 @@ const Title = styled.p`
   font-size: 60px;
   color: black;
   text-align: center;
+  font-weight: 600;
+  animation: ${easeIn} 2s ease-in;
 `;
 
