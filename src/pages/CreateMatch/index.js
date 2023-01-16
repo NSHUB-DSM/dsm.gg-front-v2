40e54765ee1f3c052ecde6795f7ac7f8f6db0b90
조우상 <woosang3078@hanmail.net>
2023-01-16 15:13:15 +0900

    ⚡ :: 배경 이미지 추가

--- src/pages/CreateMatch/index.js
+++ src/pages/CreateMatch/index.js
@@ -1,9 +1,23 @@
+import styled, { keyframes } from "styled-components";
 import Header from "../../components/Header/header";
+import BackImage from '../../assets/svgs/back_image.svg';
+import useScrollFadeIn from "../Main/hooks";
 
 export default function CreateMatch() {
-    return (
-        <>
-          <Header />
-        </>
-    )
+  const animationItem = useScrollFadeIn();
+
+  return (
+    <>
+      <Header />
+      <BackgroundImage {...animationItem}>
+        <img src={BackImage} alt="배경 이미지" />
+      </BackgroundImage>
+    </>
+  );
 }
+
+const BackgroundImage = styled.div`
+  width: 100%;
+  height: 100%;
+  transform: 0.5s ease-in-out;
+`;
