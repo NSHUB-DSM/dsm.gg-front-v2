1838c8ceb11d843f0562ee748a03ff0f7b29e70b
조우상 <woosang3078@hanmail.net>
2023-01-17 09:38:37 +0900

    ⚡ :: 매칭 생성 상세 페이지 추가

---
+++ src/pages/CreateMatch/make/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../../../components/Header/header";
+
+export default function Make() {
+
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
