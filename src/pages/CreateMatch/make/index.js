1cedb70fbdac649796e81f867841daca199fb0b2
조우상 <woosang3078@hanmail.net>
2023-01-17 14:39:16 +0900

    ♻️ :: 불필요한 코드 제거

--- src/pages/CreateMatch/make/index.js
+++ src/pages/CreateMatch/make/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import styled from "styled-components";
 import Header from "../../../components/Header/header";
-import SelectOptions from "../../../components/CreateMatch/make";
+import SelectOption from "../../../components/CreateMatch/make";
+import useTitle from "../../../components/Title/hook";
 
 export default function Make() {
+    const titleUpdater = useTitle("불러오는 중...");
+    setTimeout(() => titleUpdater("매칭 생성 상세 - DSM.GG"))
 
     return (
         <>
             <Header />
-            <SelectOptions />
+            <SelectOption />
         </>
     )
 }
