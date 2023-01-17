8362fec4805e39544b19567dfd69ea290b3f0e25
조우상 <woosang3078@hanmail.net>
2023-01-17 10:28:36 +0900

    ⚡ :: 매칭 생성 항목 선택 컴포넌트 추가

--- src/pages/CreateMatch/make/index.js
+++ src/pages/CreateMatch/make/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../../../components/Header/header";
+import SelectOptions from "../../../components/CreateMatch/make";
 
 export default function Make() {
 
     return (
         <>
             <Header />
+            <SelectOptions />
         </>
     )
 }
