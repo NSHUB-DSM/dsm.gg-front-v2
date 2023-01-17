e6f2b82182c33ee58c1ec15c41a2bdd97883fb06
조우상 <woosang3078@hanmail.net>
2023-01-17 11:45:59 +0900

    ♻️ :: 불필요한 코드 제거

--- src/components/CreateMatch/make/index.js
+++ src/components/CreateMatch/make/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import Header from "../../Header/header";
 import useScrollFadeIn from "../../../pages/Main/hooks";
 
 const SelectOptions = () => {
