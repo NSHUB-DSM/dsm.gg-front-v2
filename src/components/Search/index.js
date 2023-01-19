647cce509194ddf5bdf580d9ac9d21994b66e242
조우상 <woosang3078@hanmail.net>
2023-01-19 12:24:34 +0900

    ⚡ :: 검색하기 버튼 추가

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,15 +3,44 @@ import styled from "styled-components";
 import { useState } from "react";
 
 export default function Search() {
+  const [value, setValue] = useState("");
+  const [isInput, setIsInput] = useState(false);
+  const onChange = (e) => {
+    e.preventDefault();
+    if (e.target.value === "") {
+      setIsInput(false);
+    } else {
+      setIsInput(true);
+    }
+    setValue(e.target.value);
+  };
+
   return (
     <>
       <InputContainer>
-        <SearchInput placeholder="소환사의 닉네임을 입력하세요." />
+        <SearchInput
+          placeholder="소환사의 닉네임을 입력하세요."
+          value={value}
+          onChange={onChange}
+        />
+        {isInput && <SearchBtn>검색하기</SearchBtn>}
       </InputContainer>
     </>
   );
 }
 
+const SearchBtn = styled.p`
+  font-size: 15px;
+  color: #D3D3D3;
+  font-weight: 550;
+  text-align: right;
+  margin-top: 5px;
+  cursor: pointer;
+  transition: color 0.5s ease-in-out;
+  &:hover {
+    color: black;
+  }
+`;
 
 const InputContainer = styled.div`
   width: 660px;
