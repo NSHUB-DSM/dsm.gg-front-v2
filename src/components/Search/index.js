3c2314d0fada99d745fa4029c1edfec7273a54da
조우상 <woosang3078@hanmail.net>
2023-01-19 11:08:56 +0900

    🔨 :: 컴포넌트 구조 수정

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useState } from 'react';
-
+import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
 
 export default function Search() {
-    return (
-        <>
-          <InputContainer>
-            <SearchInput placeholder="소환사의 닉네임을 입력하세요." />
-          </InputContainer>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <InputContainer>
+          <SearchInput placeholder="소환사의 닉네임을 입력하세요." />
+        </InputContainer>
+      </div>
+    </>
+  );
 }
 
 const InputContainer = styled.div`
   width: 660px;
   height: 65px;
   margin: 0 auto;
-`
+`;
 
 const SearchInput = styled.input`
   width: 660px;
   height: 65px;
-  border: 1px solid #B2A1D4;
+  border: 1px solid #b2a1d4;
   border-radius: 7px;
   box-shadow: 1px 1px 1px gray;
   font-size: 18px;
   padding-left: 30px;
-  
-`
+`;
