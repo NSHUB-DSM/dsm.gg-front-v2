1408c7995a4b969588f1f1a52187412e3856de2f
조우상 <woosang3078@hanmail.net>
2023-01-27 10:46:20 +0900

    ⚡ :: 페이지 탭 제목 할당

--- src/pages/Matching/index.js
+++ src/pages/Matching/index.js
@@ -1,10 +1,22 @@
 import Header from "../../components/Header/header";
 import styled from "styled-components";
+import useTitle from "../../components/Title/hook";
 
 export default function Matching() {
+    const titleUpdater = useTitle("불러오는 중...")
+    setTimeout(() => titleUpdater("매칭 찾기 | DSM.GG"))
+
     return (
         <>
             <Header />
+            <Title>매칭 찾기</Title>
         </>
     )
 }
+
+const Title = styled.p`
+  font-size: 50px;
+  font-weight: 600;
+  text-align: center;
+  margin-top: 94px;
+`
