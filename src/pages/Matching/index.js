3c8d82f4f5321edfa0e78cda8544521c043bde5a
조우상 <woosang3078@hanmail.net>
2023-01-27 10:27:41 +0900

    ⚡ :: 매칭 찾기 페이지 추가

---
+++ src/pages/Matching/index.js
@@ -0,0 +1,10 @@
+import Header from "../../components/Header/header";
+import styled from "styled-components";
+
+export default function Matching() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
