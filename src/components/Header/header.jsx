216a6ed6fbcdf5b323e0e3c48bae97aaa0d2a7a7
조우상 <woosang3078@hanmail.net>
2023-02-06 11:06:43 +0900

    ⚡ :: input 입력 state 추가

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -9,6 +9,7 @@ import LoginModal from "../../pages/Main/modal/login";
 const Header = () => {
   const navigate = useNavigate();
   const [modalOpen, setModalOpen] = useState(false);
+  const [isInput, setIsInput] = useState(false);
 
   return (
     <>
