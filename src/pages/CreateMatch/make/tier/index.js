94bd1e848feb85954813b9fc8f78ad9185595912
조우상 <woosang3078@hanmail.net>
2023-02-07 12:30:10 +0900

    ⚡ :: 페이지 탭 제목 할당

--- src/pages/CreateMatch/make/tier/index.js
+++ src/pages/CreateMatch/make/tier/index.js
@@ -1,11 +1,15 @@
 import Header from "../../../../components/Header/header";
 import useTitle from "../../../../components/Title/hook";
+import TierOptions from "../../../../components/CreateMatch/make/tier";
 
 export default function TierLimit() {
+    const titleUpdater = useTitle("불러오는 중...");
+    setTimeout(() => titleUpdater("티어 제한 선택 | DSM.GG"))
     
     return (
         <>
           <Header />
+          <TierOptions />
         </>
     )
 }
