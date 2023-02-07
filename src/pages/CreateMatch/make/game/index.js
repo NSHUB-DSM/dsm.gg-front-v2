a647d5cce7b13bcc9629d3eccc6dc32f25372117
조우상 <woosang3078@hanmail.net>
2023-02-07 12:02:39 +0900

    🔨 :: 페이지 탭 제목 수정

--- src/pages/CreateMatch/make/game/index.js
+++ src/pages/CreateMatch/make/game/index.js
@@ -5,7 +5,7 @@ import useTitle from "../../../../components/Title/hook";
 
 export default function Make() {
     const titleUpdater = useTitle("불러오는 중...");
-    setTimeout(() => titleUpdater("매칭 생성 상세 | DSM.GG"))
+    setTimeout(() => titleUpdater("게임 종류 선택 | DSM.GG"))
 
     return (
         <>
