ff0f1c621d595a02d5a57d062e9dcf824c3dca12
조우상 <woosang3078@hanmail.net>
2023-02-07 12:15:19 +0900

    ⚡ :: 헤더 추가

---
+++ src/pages/CreateMatch/make/tier/index.js
@@ -0,0 +1,11 @@
+import Header from "../../../../components/Header/header";
+import useTitle from "../../../../components/Title/hook";
+
+export default function TierLimit() {
+    
+    return (
+        <>
+          <Header />
+        </>
+    )
+}
