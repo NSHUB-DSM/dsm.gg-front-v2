9f853b008fd5271e16d2db712b15c79bdb20b3f0
조우상 <woosang3078@hanmail.net>
2023-02-08 15:13:55 +0900

    ⚡ :: 인원 선택 컴포넌트 추가

--- src/pages/CreateMatch/make/people/index.js
+++ src/pages/CreateMatch/make/people/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import PeopleOptions from "../../../../components/CreateMatch/make/people";
 import Header from "../../../../components/Header/header";
 
 export default function SelectPeople() {
     return (
         <>
           <Header />
+          <PeopleOptions />
         </>
     )
 }
