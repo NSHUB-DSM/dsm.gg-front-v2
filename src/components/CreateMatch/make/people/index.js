a091031f8f2d242aae37d69c8c55f377a550c458
조우상 <woosang3078@hanmail.net>
2023-02-08 15:17:47 +0900

    ⚡ :: 더미 데이터 추가

---
+++ src/components/CreateMatch/make/people/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+const PeopleOptions = () => {
+    const peopleList = [
+        {
+            id: 0,
+            name: '선택하기'
+        },
+        {
+            id: 1,
+            people: 1
+        },
+        {
+            id: 2,
+            people: 2
+        },
+        {
+            id: 3,
+            people: 3
+        },
+        {
+            id: 4,
+            people: 4
+        }
+    ]
+
+    const [people, setPeople] = useState(0);
+    const [isSelect, setIsSelect] = useState(false);
+    const navigate = useNavigate();
+
+    return (
+        <>
+          
+        </>
+    )
+}
+
+export default PeopleOptions;
