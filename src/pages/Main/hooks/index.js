e1416cdd3acf1c07d384cf6f72457dba586d5869
조우상 <woosang3078@hanmail.net>
2023-02-08 10:40:59 +0900

    🔨 :: 애니메이션 구현시간 수정

--- src/pages/Main/hooks/index.js
+++ src/pages/Main/hooks/index.js
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useRef } from "react";
 
-const useScrollFadeIn = (direction = "up", duration = 1, { delay }) => {
+const useScrollFadeIn = (direction = "up", duration = 1, delay = 1) => {
   const dom = useRef();
 
   const handleDirection = (name) => {
